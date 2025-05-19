606127701250d29c9a8cf25b5192f786df865981
nayanbecker <nayan@unochapeco.edu.br>
2025-05-18 21:14:07 -0300

    Refactor code structure for improved readability and maintainability

--- blog-frontend/src/App.tsx
+++ blog-frontend/src/App.tsx
@@ -1,35 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Feed } from './pages/Feed'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <div className="bg-gray-100 min-h-screen flex flex-col items-center justify-center">
+            <div className="bg-gray-800 min-h-screen w-[40rem] flex flex-col items-center justify-center">
+                <Feed />
+            </div>
+        </div>
+
+    )
 }
 
 export default App
---
+++ blog-frontend/src/pages/Feed.tsx
@@ -0,0 +1,54 @@
+// src/pages/Feed.tsx
+import React, { useEffect, useState } from 'react';
+import { Post } from '../components/Posts';
+
+interface PostData {
+    id_Post: number;
+    title_Post?: string;
+    content_Post?: string;
+    image_Post?: string;
+    createdAt_Post: string;
+    perfil: {
+        nome_Perfil: string;
+        foto_Perfil: string;
+    };
+}
+
+
+export const Feed: React.FC = () => {
+    const [posts, setPosts] = useState<PostData[]>([]);
+
+    useEffect(() => {
+        fetch('http://localhost:3333/posts/', {
+            headers: {
+                authorization: `Bearer ${('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZF9BY2NvdW50IjoxMywiaWF0IjoxNzQ3NjAyMzQ0LCJleHAiOjE3NDgyMDcxNDR9.uPl3vBbhojHFhf6hNDk2Wb-4N1Fl2LckY6VpmA0paSw')}`,
+            },
+        })
+            .then(res => res.json())
+            .then(data => setPosts(data))
+            .catch(err => console.error('Erro ao buscar posts', err));
+    }, []);
+
+    return (
+        <div className="max-w-2xl mx-auto px-4 py-6">
+            <h1 className="text-2xl font-bold mb-6">Feed</h1>
+            {posts.length > 0 ? (
+                posts.map(post => (
+                    <Post
+                        key={post.id_Post}
+                        title_Post={post.title_Post}
+                        content_Post={post.content_Post}
+                        image_Post={post.image_Post}
+                        createdAt_Post={(post.createdAt_Post)}
+                        perfil={{
+                            nome_Perfil: post.perfil.nome_Perfil,
+                            foto_Perfil: post.perfil.foto_Perfil
+                        }}
+                    />
+                ))
+            ) : (
+                <p className="text-center text-gray-500">Nenhuma publicação ainda.</p>
+            )}
+        </div>
+    );
+};
